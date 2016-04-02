83f2446c53da10b929a7eca13243d0834217aff2
mwytock0812 <michael.wytock@gmail.com>
2016-04-01 21:08:55 -0400

    Add tide for TypeScript.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -14,8 +14,6 @@
 (require 'ein)
 (setq ein:use-auto-complete t)
 
-(provide 'init-local)
-
 ;;; Less obtrusive visible bell
 (defun my-terminal-visible-bell ()
   "A friendlier visual bell effect."
@@ -25,4 +23,31 @@
 (setq visible-bell       nil
       ring-bell-function #'my-terminal-visible-bell)
 
+;;; Load tide for TypeScript
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode +1)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode +1)
+            ;; company is an optional dependency. You have to
+            ;; install it separately via package-install
+            (company-mode-on)))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; Tide can be used along with web-mode to edit tsx files
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (tide-setup)
+              (flycheck-mode +1)
+              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+              (eldoc-mode +1)
+              (company-mode-on))))
+
+(provide 'init-local)
 ;;; init-local.el ends here
